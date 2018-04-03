653ea103551b0e74214ce65112141c1c47ddd803
Jacob Wong <jdw2@ualberta.ca>
2018-04-02 19:54:09 -0600

    🏳️
    surrender

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import pygame, sys, time, random
 from pygame.locals import *
-# from graph import Graph
-# from binaryHeap import BinaryHeap
 from leastCostPath import *
 
 # User-defined classes
 class Player:
-    chefColor = pygame.Color('white')
-    width = 100
-    height = 100
-    startX = 100
-    startY = 100
     def __init__(self, surface):
         self.surface = surface
-        self.rect = [Player.startX, Player.startY, Player.width, Player.height]
-        self.color = Player.chefColor
+        self.rect = Rect(102, 102, 98, 98)
+        self.color = pygame.Color('white')
         self.holding = False
 
     def draw(self):
-        pygame.draw.rect(self.surface, self.color, self.rect, 0)
+        pygame.draw.rect(self.surface, self.color, self.rect)
 
     def isHolding(self):
         self.holding = True
@@ -27,74 +20,80 @@ class Player:
         self.holding = False
 
 class Game:
-    backColor = pygame.Color('black')
-
-    def __init__(self, surface):
+    def __init__(self, surface, charColor, backColor):
         self.surface = surface
+        self.exit = False
+        self.cont = True
+        self.charcolor = charColor
+        self.backcolor = backColor
+        self.player = Player(self.surface)
 
     def draw(self):
-        player = Player(self.surface)
-        player.draw()
+        self.player.draw()
+        pygame.display.update()
+
+    def playGame(self):
+        self.draw()
+        # clock = pygame.time.Clock()
+        # counter, text = 60, '60'.rjust(2)
+        # pygame.time.set_timer(pygame.USEREVENT, 1000)
+        # font = pygame.font.SysFont('Consolas', 10)
+        while not self.exit:
+            self.events()
+            if self.cont:
+                # for event in pygame.event.get():
+                #     if event.type == pygame.USEREVENT:
+                #         counter -= 1
+                #         if counter > 0:
+                #             text = str(counter).rjust(2)
+                #         else:
+                #             text = 'game'.rjust(2)
+                #         self.surface.blit(font.render(text, True, (255,255,255)), (750,50))
+                #         pygame.display.flip()
+                #         clock.tick(60)
+                self.update()
+                # Check cond??
+            self.draw()
+            time.sleep(0.01)
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.exit = True
+            if event.type == MOUSEBUTTONUP and self.cont:
+                pygame.draw.rect(self.surface, self.backcolor,self.player.rect, 0)
+                x, y = pygame.mouse.get_pos()
+                x -= (x % 100) - 50
+                y -= (y % 100) - 50
+                # self.player.startX = x - 48
+                # self.player.startY = y - 48
+                # self.player.draw(self.charcolor)
 
     def update(self):
-        if False:
-            return True
-        else:
-            self.draw()
-            return False
+        # Put the customers timer here
+        # Put the cooking/cutting timer here
+        pass
 def main():
     # Initialize pygame
     pygame.init()
 
-    # Set window size and title, and frame delay
-    surfaceSize = (800, 600)
-    Title = 'Sandwich Queen (Not copyright)'
-    pauseTime = 0.01 # smaller is faster game
-
     # Create the window
-    surface = pygame.display.set_mode(surfaceSize, 0, 0)
-    pygame.display.set_caption(Title)
-
-    # create and initialize objects
-    gameOver = False
-    game = Game(surface)
-    player = Player(surface)
-    game.draw()
-    pygame.display.update()
-
+    surface = pygame.display.set_mode((800, 600), 0, 0)
+    backColor = pygame.Color('red')
+    charColor = pygame.Color('white')
+    pygame.display.set_caption('Sandwich Queen (Not copyright)')
     # Draw the game board grid
     for i in range(1, 8):
         pygame.draw.line(surface, pygame.Color('white'), (100*i, 0), (100*i, 600), 2)
     for j in range(0, 6):
         pygame.draw.line(surface, pygame.Color('white'), (0, 100*j), (800, 100*j), 2)
 
-    # Refresh the display
-    pygame.display.update()
-
-    # Loop forever
-    while True:
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            # Handle additional events
-            if event.type == MOUSEBUTTONUP and not gameOver:
-                pygame.draw.rect(surface, pygame.Color('black'),[player.startX + 2, player.startY + 2, player.width - 2, player.height - 2], 0)
-                x, y = pygame.mouse.get_pos()
-                x -= (x % 100) - 50
-                y -= (y % 100) - 50
-                player.startX = x - 50
-                player.startY = y - 50
-                player.draw()
-
-        # Update and draw objects for next frame
-        gameOver = game.update()
-
-        # Refresh the display
-        pygame.display.update()
-
-        # Set the frame speed by pausing between frames
-        time.sleep(pauseTime)
+    # create and initialize objects
+    gameOver = False
+    game = Game(surface, charColor, backColor)
+    # Start cooking
+    game.playGame()
+    # Quit if we get to this point
+    pygame.quit()
 
 main()
